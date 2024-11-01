367e3c6b469d0e05d08c99e3eefac83a3399edbb
vetschn <vetschnicolas@gmail.com>
2024-11-01 16:49:16 +0100

    With the new vectorized assignment, everythiong is handled automagically.

--- src/quatrex/electron/sse_phonon.py
+++ src/quatrex/electron/sse_phonon.py
@@ -62,26 +62,6 @@ class SigmaPhonon(ScatteringSelfEnergy):
         """Computes the electron-phonon self-energy."""
         return self._compute_pseudo_scattering(g_lesser, g_greater, out)
 
-    def _initialize_diag_inds(self, sigma_lesser: DSBSparse) -> None:
-        """Computes the diagonal indices for vectorized assignment."""
-        stack_padding_mask = sigma_lesser._stack_padding_mask
-        stack_padding_inds = stack_padding_mask.nonzero()[0][
-            self.downshift : -self.upshift
-        ]
-        inds = np.zeros(sigma_lesser.shape[-1], dtype=int)
-        ranks = np.zeros(sigma_lesser.shape[-1], dtype=int)
-
-        for n in range(sigma_lesser.shape[-1]):
-            inds[n] = xp.where((sigma_lesser.rows == n) & (sigma_lesser.cols == n))[0][
-                0
-            ]
-            ranks[n] = xp.where(sigma_lesser.nnz_section_offsets <= inds[n])[0][-1]
-
-        self._local_inds = (
-            inds[ranks == comm.rank] - sigma_lesser.nnz_section_offsets[comm.rank]
-        )
-        self._sigma_inds = xp.ix_(stack_padding_inds, self._local_inds)
-
     def _compute_pseudo_scattering(
         self, g_lesser: DSBSparse, g_greater: DSBSparse, out: tuple[DSBSparse, ...]
     ) -> None:
@@ -103,29 +83,48 @@ class SigmaPhonon(ScatteringSelfEnergy):
             m.dtranspose() if m.distribution_state != "nnz" else None
 
         # ==== Diagonal only ===========================================
-        if not hasattr(self, "_local_inds") or not hasattr(self, "_sigma_inds"):
-            self._initialize_diag_inds(sigma_lesser)
+        inds = xp.diag_indices(sigma_lesser.shape[-1])
 
-        sigma_lesser._data[self._sigma_inds] = self.deformation_potential**2 * (
+        sigma_lesser.stack[self.downshift : -self.upshift][
+            *inds
+        ] = self.deformation_potential**2 * (
             self.occupancy
-            * np.roll(g_lesser.data[..., self._local_inds], self.downshift, axis=0)[
-                self.totalshift :
-            ]
+            * np.roll(g_lesser[*inds], self.downshift, axis=0)[self.totalshift :]
             + (self.occupancy + 1)
-            * np.roll(g_lesser.data[..., self._local_inds], -self.upshift, axis=0)[
-                : -self.totalshift
-            ]
+            * np.roll(g_lesser[*inds], -self.upshift, axis=0)[: -self.totalshift]
         )
-        sigma_greater._data[self._sigma_inds] = self.deformation_potential**2 * (
+        sigma_greater.stack[self.downshift : -self.upshift][
+            *inds
+        ] = self.deformation_potential**2 * (
             self.occupancy
-            * np.roll(g_greater.data[..., self._local_inds], -self.upshift, axis=0)[
-                : -self.totalshift
-            ]
+            * np.roll(g_greater[*inds], -self.upshift, axis=0)[: -self.totalshift]
             + (self.occupancy + 1)
-            * np.roll(g_greater.data[..., self._local_inds], self.downshift, axis=0)[
-                self.totalshift :
-            ]
+            * np.roll(g_greater[*inds], self.downshift, axis=0)[self.totalshift :]
         )
+
+        # ==== Full matrices ===========================================
+        # nnz_stop = sigma_lesser.nnz_section_sizes[comm.rank]
+        # stack_padding_inds = sigma_lesser._stack_padding_mask.nonzero()[0][
+        #     self.downshift : -self.upshift
+        # ]
+        # sigma_lesser._data[stack_padding_inds, ..., :nnz_stop] = (
+        #     self.deformation_potential**2
+        #     * (
+        #         self.occupancy
+        #         * np.roll(g_lesser.data, self.downshift, axis=0)[self.totalshift :]
+        #         + (self.occupancy + 1)
+        #         * np.roll(g_lesser.data, -self.upshift, axis=0)[: -self.totalshift]
+        #     )
+        # )
+        # sigma_greater._data[stack_padding_inds, ..., :nnz_stop] = (
+        #     self.deformation_potential**2
+        #     * (
+        #         self.occupancy
+        #         * np.roll(g_greater.data, -self.upshift, axis=0)[: -self.totalshift]
+        #         + (self.occupancy + 1)
+        #         * np.roll(g_greater.data, self.downshift, axis=0)[self.totalshift :]
+        #     )
+        # )
 
         # Keep only the imaginary part.
         sigma_lesser._data.real = 0.0
@@ -136,34 +135,6 @@ class SigmaPhonon(ScatteringSelfEnergy):
             ...,
             : sigma_retarded.nnz_section_sizes[comm.rank],
         ] = 0.5 * (sigma_greater.data - sigma_lesser.data)
-
-        # ==== Full matrices ===========================================
-        # nnz_stop = sigma_lesser.nnz_section_sizes[comm.rank]
-
-        # sigma_lesser._data[stack_padding_inds, ..., :nnz_stop] = (
-        #     self.deformation_potential**2
-        #     * (
-        #         self.occupancy
-        #         * np.roll(g_lesser.data, self.downshift, axis=0)[self.totalshift :]
-        #         + (self.occupancy + 1)
-        #         * np.roll(g_lesser.data, -self.upshift, axis=0)[: -self.totalshift]
-        #     ).imag
-        #     * 1j
-        # )
-        # sigma_greater._data[stack_padding_inds, ..., :nnz_stop] = (
-        #     self.deformation_potential**2
-        #     * (
-        #         self.occupancy
-        #         * np.roll(g_greater.data, -self.upshift, axis=0)[: -self.totalshift]
-        #         + (self.occupancy + 1)
-        #         * np.roll(g_greater.data, self.downshift, axis=0)[self.totalshift :]
-        #     ).imag
-        #     * 1j
-        # )
-
-        # sigma_retarded._data[stack_padding_mask, ..., :nnz_stop] = 0.5 * (
-        #     sigma_greater.data - sigma_lesser.data
-        # )
 
         # Transpose the matrices back to the original stack distribution.
         for m in (g_lesser, g_greater, sigma_lesser, sigma_greater, sigma_retarded):
