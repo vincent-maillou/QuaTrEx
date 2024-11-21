c93e2b855c7b310ab1a1aac31912e75e7b9004d2
awinka <awinka@iis.ee.ethz.ch>
2024-11-21 14:23:39 +0100

    Added the creation of the reduced screened interactions to the constructur if SigmaCoulombScreening.
    Also changed the constructor of SigmaFock to create the coulomb_matrix object with the same sparsity pattern as the green's function

--- src/quatrex/electron/sse_coulomb_screening.py
+++ src/quatrex/electron/sse_coulomb_screening.py
@@ -5,6 +5,7 @@ from mpi4py.MPI import COMM_WORLD as comm
 from qttools.datastructures import DSBSparse
 from qttools.utils.gpu_utils import xp
 from qttools.utils.mpi_utils import distributed_load
+from scipy import sparse
 
 from quatrex.core.compute_config import ComputeConfig
 from quatrex.core.quatrex_config import QuatrexConfig
@@ -54,15 +55,34 @@ def hilbert_transform(a: xp.ndarray, energies: xp.ndarray, eta=1e-8) -> xp.ndarr
         fft_convolve(a, 1 / (energy_differences + eta))[:ne]
         + fft_convolve(a, 1 / (-energy_differences[::-1] - eta))[ne - 1 :]
     )
-    # Not sure about the prefactor.
+    # Not sure about the prefactor. Currently gives the same value as the old code.
     return b / (2 * np.pi) * (energies[1] - energies[0])
 
 
 class SigmaCoulombScreening(ScatteringSelfEnergy):
-    def __init__(self, electron_energies: xp.ndarray):
+    def __init__(
+        self,
+        quatrex_config: QuatrexConfig,
+        compute_config: ComputeConfig,
+        electron_energies: xp.ndarray,
+    ):
         self.energies = electron_energies
         self.ne = self.energies.size
         self.prefactor = 1j / (2 * np.pi) * (self.energies[1] - self.energies[0])
+        # Load the Hamiltonian and block sizes for sparsity pattern.
+        hamiltonian_sparray = distributed_load(
+            quatrex_config.input_dir / "hamiltonian.npz"
+        )
+        block_sizes = distributed_load(quatrex_config.input_dir / "block_sizes.npy")
+        self.w_lesser_reduced = compute_config.dbsparse_type.from_sparray(
+            hamiltonian_sparray,
+            block_sizes,
+            (self.ne,),
+            densify_blocks=[(i, i) for i in range(len(block_sizes))],
+        )
+        self.w_greater_reduced = compute_config.dbsparse_type.zeros_like(
+            self.w_lesser_reduced
+        )
 
     def compute(
         self,
@@ -76,26 +96,22 @@ class SigmaCoulombScreening(ScatteringSelfEnergy):
 
         # If the w_lesser and w_greater don't have the same sparsity pattern as
         # g_lesser and g_greater, we have to reduce them to the same sparsity pattern.
-        if w_lesser.nnz != g_lesser.nnz:
-            w_lesser_reduced = w_lesser.reduce_to(
-                g_lesser.rows, g_lesser.cols, g_lesser.block_sizes
-            )
+        if w_lesser.nnz != self.w_lesser_reduced.nnz:
+            self.w_lesser_reduced.data[:] = w_lesser[*self.w_lesser_reduced.spy()]
         else:
-            w_lesser_reduced = w_lesser
-        if w_greater.nnz != g_greater.nnz:
-            w_greater_reduced = w_greater.reduce_to(
-                g_greater.rows, g_greater.cols, g_greater.block_sizes
-            )
+            self.w_lesser_reduced.data[:] = w_lesser.data
+        if w_greater.nnz != self.w_greater_reduced.nnz:
+            self.w_greater_reduced.data[:] = w_greater[*self.w_greater_reduced.spy()]
         else:
-            w_greater_reduced = w_greater
+            self.w_greater_reduced.data[:] = w_greater.data
 
         sigma_lesser, sigma_greater, sigma_retarded = out
         # Transpose the matrices to nnz distribution.
         for m in (
             g_lesser,
             g_greater,
-            w_lesser_reduced,
-            w_greater_reduced,
+            self.w_lesser_reduced,
+            self.w_greater_reduced,
             sigma_lesser,
             sigma_greater,
             sigma_retarded,
@@ -109,16 +125,20 @@ class SigmaCoulombScreening(ScatteringSelfEnergy):
             ...,
             : sigma_lesser.nnz_section_sizes[comm.rank],
         ] += self.prefactor * (
-            fft_convolve(g_lesser.data, w_lesser_reduced.data)[: self.ne]
-            - fft_correlate(g_lesser.data, w_greater_reduced.data.conj())[self.ne - 1 :]
+            fft_convolve(g_lesser.data, self.w_lesser_reduced.data)[: self.ne]
+            - fft_correlate(g_lesser.data, self.w_greater_reduced.data.conj())[
+                self.ne - 1 :
+            ]
         )
         sigma_greater._data[
             sigma_greater._stack_padding_mask,
             ...,
             : sigma_greater.nnz_section_sizes[comm.rank],
         ] += self.prefactor * (
-            fft_convolve(g_greater.data, w_greater_reduced.data)[: self.ne]
-            - fft_correlate(g_greater.data, w_lesser_reduced.data.conj())[self.ne - 1 :]
+            fft_convolve(g_greater.data, self.w_greater_reduced.data)[: self.ne]
+            - fft_correlate(g_greater.data, self.w_lesser_reduced.data.conj())[
+                self.ne - 1 :
+            ]
         )
 
         # Compute retarded self-energy with a Hilbert transform.
@@ -136,8 +156,8 @@ class SigmaCoulombScreening(ScatteringSelfEnergy):
         for m in (
             g_lesser,
             g_greater,
-            w_lesser,
-            w_greater,
+            self.w_lesser_reduced,
+            self.w_greater_reduced,
             sigma_lesser,
             sigma_greater,
             sigma_retarded,
@@ -157,16 +177,27 @@ class SigmaFock(ScatteringSelfEnergy):
         coulomb_matrix_sparray = distributed_load(
             quatrex_config.input_dir / "coulomb_matrix.npz"
         ).astype(xp.complex128)
+        # Load block sizes for the coulomb matrix.
         block_sizes = distributed_load(quatrex_config.input_dir / "block_sizes.npy")
+        # Load the Hamiltonian to get the correct sparsity pattern.
+        hamiltonian_sparray = distributed_load(
+            quatrex_config.input_dir / "hamiltonian.npz"
+        )
+        # Create the DSBSparse object.
         self.coulomb_matrix = compute_config.dbsparse_type.from_sparray(
-            coulomb_matrix_sparray,
+            sparse.coo_matrix(
+                (
+                    xp.asarray(
+                        coulomb_matrix_sparray.tocsr()[
+                            hamiltonian_sparray.row, hamiltonian_sparray.col
+                        ]
+                    )[0],
+                    (hamiltonian_sparray.row, hamiltonian_sparray.col),
+                )
+            ),
             block_sizes,
             (self.energies.size,),
-            densify_blocks=[
-                (i, j)
-                for i in range(len(block_sizes))
-                for j in range(max(i - 1, 0), min(i + 2, len(block_sizes)))
-            ],
+            densify_blocks=[(i, i) for i in range(len(block_sizes))],
         )
 
     def compute(
@@ -185,7 +216,7 @@ class SigmaFock(ScatteringSelfEnergy):
             ...,
             : sigma_retarded.nnz_section_sizes[comm.rank],
         ] += (
-            gl_density * self.coulomb_matrix[*g_lesser.spy()]
+            gl_density * self.coulomb_matrix.data
         )
         for m in (g_lesser, sigma_retarded, self.coulomb_matrix):
             m.dtranspose() if m.distribution_state != "stack" else None
