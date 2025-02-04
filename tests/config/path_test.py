1ddefa554439abd8c85859ba4648f179dd894f1f
vetschn <vetschnicolas@gmail.com>
2025-02-04 16:41:25 +0100

    Formatting

--- tests/config/path_test.py
+++ tests/config/path_test.py
@@ -1,14 +1,18 @@
-# Copyright 2023-2025 ETH Zurich and the QuaTrEx authors. All rights reserved.
+# Copyright (c) 2024 ETH Zurich and the authors of the quatrex package.
 
 import os
-import pytest
 from pathlib import Path
-from quatrex.core.quatrex_config import parse_config
 
+import pytest
+
+from quatrex.core.quatrex_config import parse_config
 
 quatrex_folder = Path(os.path.dirname(__file__)).parent.parent
 
-@pytest.mark.parametrize("toml_filename", ["examples/w90/cnt/config.toml", "examples/cp2k/cnt/config.toml"])
+
+@pytest.mark.parametrize(
+    "toml_filename", ["examples/w90/cnt/config.toml", "examples/cp2k/cnt/config.toml"]
+)
 def test_simulation_path(toml_filename: str) -> None:
     toml_file = quatrex_folder.joinpath(toml_filename)
     print(toml_file)
