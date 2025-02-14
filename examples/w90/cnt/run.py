fe19b11b1b6aa8b878d7cbc10c6aa72618543503
vetschn <vetschnicolas@gmail.com>
2025-02-14 19:29:40 +0100

    Fix CNT phonon pseudoscattering example

--- examples/w90/cnt/run.py
+++ examples/w90/cnt/run.py
@@ -1,18 +1,23 @@
-from threadpoolctl import threadpool_limits  # isort: skip
+from threadpoolctl import threadpool_limits, threadpool_info  # isort: skip
 import os
 import time
+from pprint import pprint
 
-import numpy as np
+import numba as nb
 from mpi4py.MPI import COMM_WORLD as comm
 
 from quatrex.core.quatrex_config import parse_config
 from quatrex.core.scba import SCBA
 
 PATH = os.path.dirname(__file__)
+NUM_THREADS = 1
 
 if __name__ == "__main__":
-    with threadpool_limits(limits=1):
-        config = parse_config(os.path.join(PATH, "config.toml"))
+
+    nb.set_num_threads(NUM_THREADS)
+    with threadpool_limits(limits=NUM_THREADS):
+        pprint(threadpool_info()) if comm.rank == 0 else None
+        config = parse_config(f"{PATH}/config.toml")
         scba = SCBA(config)
         tic = time.perf_counter()
         scba.run()
@@ -20,14 +25,3 @@ if __name__ == "__main__":
 
     if comm.rank == 0:
         print(f"Leaving SCBA after: {(toc - tic):.2f} s")
-
-        output_dir = f"{PATH}/outputs"
-        try:
-            os.mkdir(output_dir)
-        except FileExistsError:
-            pass
-        np.save(f"{output_dir}/electron_ldos.npy", scba.observables.electron_ldos)
-        np.save(f"{output_dir}/electron_density.npy", scba.observables.electron_density)
-        np.save(f"{output_dir}/hole_density.npy", scba.observables.hole_density)
-        np.save(f"{output_dir}/i_left.npy", scba.observables.electron_current["left"])
-        np.save(f"{output_dir}/i_right.npy", scba.observables.electron_current["right"])
